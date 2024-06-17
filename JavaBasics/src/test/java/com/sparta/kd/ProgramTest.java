2a05b7ec7483cf229682efad0654cd7bbfcd3c7f
KianDoyleSparta <kdoyle@spartaglobal.com>
2024-06-17 15:09:55 +0100

    Added JUnit and basicTest

---
+++ JavaBasics/src/test/java/com/sparta/kd/ProgramTest.java
@@ -0,0 +1,11 @@
+package com.sparta.kd;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ProgramTest {
+    @Test
+    public void basicTest() {
+        Assertions.assertTrue(5 == 5);
+    }
+}
