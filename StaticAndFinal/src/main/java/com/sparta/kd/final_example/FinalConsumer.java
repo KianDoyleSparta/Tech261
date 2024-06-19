d44fd59ab6f27e37ebf9bb20228893bce8fcdb0e
KianDoyleSparta <kdoyle@spartaglobal.com>
2024-06-19 14:22:11 +0100

    Static and final example projects added

---
+++ StaticAndFinal/src/main/java/com/sparta/kd/final_example/FinalConsumer.java
@@ -0,0 +1,17 @@
+package com.sparta.kd.final_example;
+
+public class FinalConsumer extends FinalExample {
+
+    public static int FINAL_SCORE = 100;
+
+    @Override
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    // DON'T WORRY, THESE AREN'T MEANT TO WORK PROPERLY!
+}
---
+++ StaticAndFinal/src/main/java/com/sparta/kd/final_example/FinalExample.java
@@ -0,0 +1,11 @@
+package com.sparta.kd.final_example;
+
+public class FinalExample {
+
+    private final int score = 100;
+
+    public int getScore() {
+        return score;
+    }
+
+}
