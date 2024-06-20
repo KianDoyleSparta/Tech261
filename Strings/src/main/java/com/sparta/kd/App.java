c87a138600dd0b28bacd06c8453ed8669c306024
KianDoyleSparta <kdoyle@spartaglobal.com>
2024-06-20 11:01:27 +0100

    New String example

---
+++ Strings/src/main/java/com/sparta/kd/App.java
@@ -0,0 +1,24 @@
+package com.sparta.kd;
+
+public class App {
+    public static void main(String[] args) {
+        String string1 = "Hello";
+        String string2 = "Hello";
+        String string3 = new String("Hello");
+        String string4 = new String("Hello");
+
+        System.out.println(string1.hashCode());
+        System.out.println(string2.hashCode());
+        System.out.println(string3.hashCode());
+        System.out.println(string4.hashCode());
+        System.out.println();
+        System.out.println(string1 == string3);
+        System.out.println(string1.equals(string3));
+        System.out.println();
+        System.out.println(System.identityHashCode(string1));
+        System.out.println(System.identityHashCode(string2));
+        System.out.println(System.identityHashCode(string3));
+        System.out.println(System.identityHashCode(string4));
+
+    }
+}
