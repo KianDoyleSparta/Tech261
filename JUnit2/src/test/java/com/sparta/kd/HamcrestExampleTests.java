4081572308e38cf60fd3bdf085eb1af5736c8c33
KianDoyleSparta <kdoyle@spartaglobal.com>
2024-06-27 15:23:08 +0100

    Added some more tests to spartan hamcrest example

--- JUnit2/src/test/java/com/sparta/kd/HamcrestExampleTests.java
+++ JUnit2/src/test/java/com/sparta/kd/HamcrestExampleTests.java
@@ -40,6 +40,36 @@ public class HamcrestExampleTests {
         void checkThatSpartanHasAFieldCalledCourseSetToJava() {
             assertThat(manish, hasProperty("course", equalTo("Java")));
         }
+
+        @Test
+        @DisplayName("Check that spartan has a field called course set to a valid course")
+        void checkThatSpartanHasAFieldCalledCourseAndIsSetToAValidCourse() {
+            assertThat(manish, hasProperty("course", either(is("Java")).or(is("C#")).or(is("Data")).or(is("DevOps")).or(is("Cyber-Security")).or(is("Business")).or(is("Test"))));
+        }
+
+        @Test
+        @DisplayName("Check that spartan has a field called course set to a valid course better")
+        void checkThatSpartanHasAFieldCalledCourseAndIsSetToAValidCourseBetter() {
+            assertThat(manish, hasProperty("course", oneOf("Java", "C#", "Data", "DevOps", "Cyber-Security", "Business", "Test")));
+        }
+
+        @Test
+        @DisplayName("Check that spartan has a field called id set to 1")
+        void checkThatSpartanHasAFieldCalledIdAndIsSetTo1() {
+            assertThat(manish, hasProperty("id", equalTo(1)));
+        }
+//        @Test
+//        @DisplayName("Check that spartan has a field called id and is a valid int")
+//        void checkThatSpartanHasAFieldCalledIdAndIsAValidInt() {
+//            assertThat(manish, hasProperty("id", greaterThanOrEqualTo(1).and(lessThanOrEqualTo(10000)));
+//        }
+
+        @Test
+        @DisplayName("Check that spartan has a field called startDate")
+        void checkThatSpartanHasAFieldCalledStartDate() {
+            assertThat(manish, hasProperty("startDate"));
+        }
+
     }
 
     @Nested
